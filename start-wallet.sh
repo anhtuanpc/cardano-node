ac18e8923d8b02be3a0ba78decada474686cd05b
TonyDao <tuanda1@vmodev.com>
2022-11-11 11:11:01 +0700

    change wallet script for preprod

--- start-wallet.sh
+++ start-wallet.sh
@@ -1,8 +1,6 @@
 echo
-cardano-node run \
-  --topology topology.json \
-  --database-path db \
-  --socket-path db/node.socket \
-  --host-addr 0.0.0.0 \
-  --port 3001 \
-  --config config.json
+cardano-wallet serve --port 8090 \
+  --node-socket db/node.socket \
+  --testnet byron-genesis.json \
+  --database ./wallet-db \
+  --token-metadata-server https://metadata.cardano-testnet.iohkdev.io
